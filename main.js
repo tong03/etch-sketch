a1e09311d3576bf6be4fdcfa73dc9fb5c28d7643
tritong <tongtri31@gmail.com>
2023-07-06 12:41:27 -0700

    Dynamic Grid Change implementation
    Finally implemented variable dimension grid change, I had trouble
    fitting the right number of grid divs on each row and the grid divs
    staying inside the container border. Tried messing around with the flex
    attribute and also giving width and height variability to each
    grid divs. Turns out I shouldn't have done both and rather only needed
    to give variable width and height to each grid div for them to fit
    properly inside the container div. I also realized that I needed to
    assign box-sizing: border-box so that the border size of each individual
    grid div isn't affecting the alignments on each row.

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-function makeGrid () {
-    for(let i=0; i<16;i++){
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        grid.textContent = 'o';
-        container.appendChild(grid);   
+function makeGrid (dim) {
+    for(let i=0; i<dim;i++){
+        for (let j=0; j<dim;j++){
+            const grid = document.createElement('div');
+            // add grid class to div for CSS styling
+            grid.classList.add('grid');
+            grid.style.width = `${500/dim}px`;
+            grid.style.height = `${500/dim}px`;
+            console.log(grid.style.width);
+            // add hover effect to each div with event listener
+            grid.addEventListener('mousedown', (e) => {
+                // mouse down changes color
+                e.target.style.backgroundColor = 'black';
+                console.log(e.target);
+                // mousedown and drag also changes color
+                this.addEventListener('mouseover', colorChange);
+            });
+            grid.addEventListener('mouseup', (e) => {
+                this.removeEventListener('mouseover', colorChange);
+            });
+            // put these divs inside container div
+            container.appendChild(grid);   
+        }
     }
 }
+
 
 function colorChange(e) {
     // only change color if target has class "grid" in it
@@ -17,11 +35,17 @@ function colorChange(e) {
 
 function updateGrid() {
     let selection = parseInt(prompt('Enter desired grid dimensions: '));
+    // clear previous grid before initializing new one
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
     // run grid function with new INT input
+    makeGrid(selection);
 }   
 
 const container = document.querySelector('.container');
 const gridBTN = document.querySelector('.makeG');
+/*
 // Make a 16x16 grid of divs
 for(let i=0; i<16;i++){
     for (let j=0; j<16;j++){
@@ -42,6 +66,7 @@ for(let i=0; i<16;i++){
         // put these divs inside container div
         container.appendChild(grid);   
     }
-}
+} */
 
+makeGrid(16);
 gridBTN.addEventListener('click', updateGrid);
