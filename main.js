b0af79a923a0c20d13a81f9381007a1667b3e10c
tritong <tongtri31@gmail.com>
2023-07-07 16:56:09 -0700

    Reformatted the color fill implementation
    Separated the grids event listener from the grid initialization.
    Then further separated the fill color choices from the event listeners
    into just one function. This help simplifies the color changes feature by
    simply needing to change the colorFill function. With this, I was able
    to implement some version of rainbow fill, albeit there are way more
    shadings and color variations at the moment than an actual rainbow.

--- main.js
+++ main.js
@@ -6,23 +6,12 @@ function makeGrid (dim) {
             grid.classList.add('grid');
             grid.style.width = `${500/dim}px`;
             grid.style.height = `${500/dim}px`;
-            // add hover effect to each div with event listener
-            grid.addEventListener('mousedown', (e) => {
-                // mouse down changes color
-                e.target.style.backgroundColor = 'black';
-                console.log(e.target);
-                // mousedown and drag also changes color
-                this.addEventListener('mouseover', colorChange);
-            });
-            grid.addEventListener('mouseup', (e) => {
-                this.removeEventListener('mouseover', colorChange);
-            });
             // put these divs inside container div
             container.appendChild(grid);   
         }
     }
 }
-
+/*
 function colorChange(e) {
     // only change color if target has class "grid" in it
     // this should limit color change to only the divs
@@ -46,19 +35,7 @@ function randomMode() {
         });
     });
 }
-
-function randomColor(e) {
-    // only change color if target has class "grid" in it
-    // this should limit color change to only the divs
-    if(e.target.classList.value == 'grid'){
-        let val1 = Math.round(Math.random() * 255);
-        let val2 = Math.round(Math.random() * 255);
-        let val3 = Math.round(Math.random() * 255);
-        let rColor = `rgb(${val1}, ${val2}, ${val3})`;
-
-        e.target.style.backgroundColor = rColor;
-    }
-}
+*/
 
 function updateGrid() {
     let selection = parseInt(prompt('Enter desired grid dimensions: '));
@@ -73,6 +50,8 @@ function updateGrid() {
     makeGrid(selection);
 }   
 
+let defaultC = true;
+let rainbow = false;
 const container = document.querySelector('.container');
 const gridBTN = document.querySelector('.makeG');
 const rColorBTN = document.querySelector('.rand');
@@ -81,4 +60,52 @@ const rColorBTN = document.querySelector('.rand');
 makeGrid(16);
 // Change grid to specified dimensions each time
 gridBTN.addEventListener('click', updateGrid);
-rColorBTN.addEventListener('click', randomMode);
+rColorBTN.addEventListener('click', () => {
+    defaultC = false;
+    rainbow = true;
+})
+// rColorBTN.addEventListener('click', randomMode);
+
+/*
+    Select collection of grids with 'grid' class
+
+    Loop through the grid collection and add event listener for each
+    that would run colorFill() with mousedown/mouseover/mouseup
+
+ */
+let grids = document.getElementsByClassName('grid');
+Array.from(grids).forEach((grid) => {
+    grid.addEventListener('mousedown', (e) => {
+        // mouse down changes color
+        colorFill(e);
+        // mousedown and drag also changes color
+        this.addEventListener('mouseover', colorFill);
+    });
+    grid.addEventListener('mouseup', () => {
+        this.removeEventListener('mouseover', colorFill);
+    });
+});
+
+// This function should only check for the correct active tags and react
+// correspondingly to fill in color
+function colorFill(e) {
+    if(defaultC){
+        if(e.target.classList.value == 'grid'){
+            e.target.style.backgroundColor = 'black';
+        }
+    }
+    else if (rainbow){
+        if(e.target.classList.value == 'grid'){
+            e.target.style.backgroundColor = randomColor();
+        }
+    }
+}
+
+function randomColor() {
+    let val1 = Math.round(Math.random() * 255);
+    let val2 = Math.round(Math.random() * 255);
+    let val3 = Math.round(Math.random() * 255);
+
+    let rColor = `rgb(${val1}, ${val2}, ${val3})`;
+    return rColor; 
+}
